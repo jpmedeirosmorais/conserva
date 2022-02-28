d2d5af783241e1034b5c56882681ad64d10002bc
João P M Morais <paulomedeiros5000@gmail.com>
2022-02-28 05:02:39 -0300

    create button component

---
+++ stories/Button.stories.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Button } from "../components";
+
+export default {
+  title: "components/Button",
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  label: "Clique",
+};
